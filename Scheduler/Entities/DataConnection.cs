fa84e20ac8500450af4373f92864dc1ed3fa676a
Lucas Cerqueira Santos <lucaas.cerqueirasantos@gmail.com>
2022-12-28 12:14:29 +0000

    Folder Entities was created

---
+++ Scheduler/Entities/DataConnection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+
+namespace Scheduler.Entities
+{
+    internal class Data_Connections
+    {
+        //const string connectionString = @"Server=DESKTOP-1FA761O;Database=master;Trusted_Connection=True;";
+        const string connectionString = @"Server=DESKTOP-1FA761O;Database=Scheduler;Trusted_Connection=True;";
+        //Data Source = DESKTOP - 1FA761O;I
+        private SqlConnection sqlConnection = new(connectionString);
+
+        public void ExcuteSQL(string command_text, List<SqlParameter> parameters = null)
+        {
+            SqlCommand command = sqlConnection.CreateCommand();
+
+            if (parameters != null)
+            {
+                foreach (SqlParameter parameter in parameters)
+                {
+                    command.Parameters.Add(parameter);
+                }
+            }
+            command.CommandText = command_text;
+            sqlConnection.Open();
+            command.ExecuteNonQuery();
+            sqlConnection.Close();
+        }
+    }
+}
